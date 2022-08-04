6140fe98b7a93b780b974384b4a9bd479a8fd4c4
Volodymyr Savko <v.savko@dinamicka.com>
2022-08-04 23:56:56 +0300

    first Rest API

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,14 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+
 app.get('/', (req:unknown, res:{send:(msg:string)=>void}) => {
     res.send('Hello World!')
 })
+app.get("/damn", (req, res)=> {
+    res.send({message:"damn"})
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
